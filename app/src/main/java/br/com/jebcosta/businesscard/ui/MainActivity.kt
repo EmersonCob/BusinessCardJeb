109ff7f26a37ef2fbb779a6f0f1524c391d7478a
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-08-08 16:03:07 -0300

    Update

--- app/src/main/java/br/com/jebcosta/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/jebcosta/businesscard/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 package br.com.jebcosta.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.jebcosta.businesscard.R
 import br.com.jebcosta.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
